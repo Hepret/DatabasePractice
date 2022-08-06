bbf132dfa0f33d4ece42b2e8a2904340d1f7a32c
Daniel Saulin <saulin.daniel33@gmail.com>
2022-08-06 16:27:17 +0300

    Add classes that store scripts for inserting data in database

---
+++ DatabaseCopierSingle/ScriptCreators/ScriptForInsertData/DataInsertScripts.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DatabaseCopierSingle.ScriptCreators.ScriptForInsertData
+{
+    public class DataInsertScripts : IEnumerable
+    {
+        public List<string> Scripts { get; private set; }
+        public DataInsertScripts(IEnumerable<string> scripts)
+        {
+            Scripts = scripts.ToList();
+        }
+        public DataInsertScripts()
+        {
+            Scripts = new List<string>();
+        }
+        public void AddRange(IEnumerable<string> scripts)
+        {
+            Scripts.AddRange(scripts);
+        }
+        public void Add(string script)
+        {
+            Scripts.Add(script);
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return Scripts.GetEnumerator();
+        }
+    }
+}
---
+++ DatabaseCopierSingle/ScriptCreators/ScriptForInsertData/InsertDataInTableScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DatabaseCopierSingle.DatabaseTableComponents;
+
+namespace DatabaseCopierSingle.ScriptCreators.ScriptForInsertData
+{
+    public class InsertDataInTableScript : IEnumerable
+    {
+        private readonly SchemaTable _schemaTable;
+
+        public string TableName => _schemaTable.TableName;
+
+        private List<string> InsertDataInScripts { get; set; }
+        
+        public InsertDataInTableScript(SchemaTable schemaTable)
+        {
+            _schemaTable = schemaTable;
+            InsertDataInScripts = new List<string>();
+        }
+
+        public string this[int index] => InsertDataInScripts[index];
+
+
+        public void AddScript(string script)
+        {
+            InsertDataInScripts.Add(script);
+        }
+
+        public void AddScripts(IEnumerable<string> scripts)
+        {
+            InsertDataInScripts.AddRange(scripts);
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return InsertDataInScripts.GetEnumerator();
+        }
+    }
+}
