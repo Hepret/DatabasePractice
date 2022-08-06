dc9cb59076bccc7400470fcc4ba7c1d19ab0820d
Daniel Saulin <saulin.daniel33@gmail.com>
2022-08-06 16:57:01 +0300

    Add Equals method to FullTableName

---
+++ DatabaseCopierSingle/DatabaseTableComponents/SchemaTableComponents/FullTableName.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DatabaseCopierSingle.DatabaseTableComponents.SchemaTableComponents
+{
+    public class FullTableName
+    {
+        public string TableName { get; set; }
+        public string SchemaCatalogName { get; set; }
+        protected bool Equals(FullTableName other)
+        {
+            return TableName == other.TableName && SchemaCatalogName == other.SchemaCatalogName;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((FullTableName) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(TableName, SchemaCatalogName);
+        }
+        
+    }
+}
