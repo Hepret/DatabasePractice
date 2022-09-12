044132e3afaf6dc5bf7735a705916fa585ad0321
Daniel Saulin <saulin.daniel33@gmail.com>
2022-09-12 15:56:54 +0300

    Добавил класс для создание классов копирования

---
+++ DatabaseCopierSingle/DatabaseCopiers/DatabaseCopierFactory.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using DatabaseCopierSingle.DatabaseCopiers.DatabaseDataReceivers;
+using DatabaseCopierSingle.DatabaseProviders;
+
+namespace DatabaseCopierSingle.DatabaseCopiers
+{
+
+    public static class DatabaseCopierFactory
+    {
+        public static DatabaseCopier CreateCopier(string connectionStringFrom, Database databaseFrom, string connectingStringTo, Database databaseTo, bool needToCreateNewDatabase)
+        {
+            DatabaseCopier copier;
+            switch (databaseFrom)
+            {
+                case Database.Mssql when databaseTo == Database.Mssql:
+                    copier = new CopyMssqlToMssql(connectionStringFrom, connectingStringTo, needToCreateNewDatabase);
+                    break;
+                case Database.Mssql when  databaseTo == Database.Postgresql:
+                    copier = new CopyMssqlToPostgresql(connectionStringFrom, connectingStringTo, needToCreateNewDatabase);
+                    break;
+                case Database.Postgresql when databaseTo == Database.Mssql:
+                    copier = new CopyPostgresqlToMssql(connectionStringFrom, connectingStringTo, needToCreateNewDatabase);
+                    break;
+                case Database.Postgresql when databaseTo == Database.Postgresql:
+                    copier = new CopyPostgresqlToPostgresql(connectionStringFrom, connectingStringTo,
+                        needToCreateNewDatabase);
+                    break;
+                default:
+                    throw  new Exception();
+            }
+
+            return copier;
+        }
+    }
+    public enum Database
+    {
+        Mssql,
+        Postgresql
+    }
+    
+}
