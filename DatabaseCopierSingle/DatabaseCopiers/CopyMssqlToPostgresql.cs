7f8f516b6b09b113233f609fd3ceddc869387f5b
Daniel Saulin <saulin.daniel33@gmail.com>
2022-09-12 15:51:18 +0300

    Добавил класс для копирования из mssql в postgresql, класс для копирования из postgresql в mssql

---
+++ DatabaseCopierSingle/DatabaseCopiers/CopyMssqlToPostgresql.cs
@@ -0,0 +1,25 @@
+﻿using DatabaseCopierSingle.DatabaseCopiers.DatabaseDataReceivers;
+using DatabaseCopierSingle.DatabaseCopiers.DatabaseSchemaReceivers;
+using DatabaseCopierSingle.DatabaseProviders;
+using DatabaseCopierSingle.ScriptCreators.DatabaseDataInsertingScriptsCreator;
+using DatabaseCopierSingle.ScriptCreators.DatabaseSchemaCreatingScriptsCreator;
+
+namespace DatabaseCopierSingle.DatabaseCopiers
+{
+    public class CopyMssqlToPostgresql : DatabaseCopier
+    {
+        public CopyMssqlToPostgresql(string connectionStringFrom, string connectionStringTo, bool needToCreateDatabase) :
+            base(new MssqlProvider(connectionStringFrom),
+                new PostgresqlProvider(connectionStringTo), needToCreateDatabase)
+        {
+            SchemaReceiver = new MssqlSchemaReceiver(ProviderFrom);
+            DataReceiver = new MssqlDataReceiver(ProviderFrom);
+
+            DatabaseSchemaInserter = new DatabaseSchemaInserter.DatabaseSchemaInserter(ProviderTo, NeedToCreateNewDatabase);
+            DatabaseDataInserter = new DatabaseDataInserter.DatabaseDataInserter(ProviderTo);
+
+            SchemaScriptsCreator = new CreatorScriptsFromSchemaMssqlToPostgresql();
+            DataScriptsCreator = new CreatorScriptsForInsertDataMssqlToPostgresql();
+        }
+    }
+}
---
+++ DatabaseCopierSingle/DatabaseCopiers/CopyPostgresqlToMssql.cs
@@ -0,0 +1,25 @@
+﻿using DatabaseCopierSingle.DatabaseCopiers.DatabaseDataReceivers;
+using DatabaseCopierSingle.DatabaseCopiers.DatabaseSchemaReceivers;
+using DatabaseCopierSingle.DatabaseProviders;
+using DatabaseCopierSingle.ScriptCreators.DatabaseDataInsertingScriptsCreator;
+using DatabaseCopierSingle.ScriptCreators.DatabaseSchemaCreatingScriptsCreator;
+
+namespace DatabaseCopierSingle.DatabaseCopiers
+{
+    public class CopyPostgresqlToMssql : DatabaseCopier
+    {
+        public CopyPostgresqlToMssql(string connectionStringFrom, string connectionStringTo, bool needToCreateDatabase) :
+            base(new PostgresqlProvider(connectionStringFrom),
+                new MssqlProvider(connectionStringTo), needToCreateDatabase)
+        {
+            SchemaReceiver = new PostgresqlSchemaReceiver(ProviderFrom);
+            DataReceiver = new PostgresqlDataReceiver(ProviderFrom);
+
+            DatabaseSchemaInserter = new DatabaseSchemaInserter.DatabaseSchemaInserter(ProviderTo, NeedToCreateNewDatabase);
+            DatabaseDataInserter = new DatabaseDataInserter.DatabaseDataInserter(ProviderTo);
+
+            SchemaScriptsCreator = new CreatorScriptsFromSchemaPostgresqlToMssql();
+            DataScriptsCreator = new CreatorScriptsForInsertDataPostgresqlToMssql();
+        }
+    }
+}
