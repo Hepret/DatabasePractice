53035e464bef7f1abbd7382185a95fb14b919589
Daniel Saulin <saulin.daniel33@gmail.com>
2022-09-12 16:47:22 +0300

    Добавил класс ошибки DatabaseConnectionFailedException.cs

---
+++ DatabaseCopierSingle/DatabaseProviders/Exceptions/DatabaseConnectionFailedException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DatabaseCopierSingle.DatabaseProviders.Exceptions
+{
+    public class DatabaseConnectionFailedException : Exception
+    {
+        public DatabaseConnectionFailedException()
+        {
+        }
+
+        public DatabaseConnectionFailedException(string message) 
+            : base(message)
+        {
+        }
+
+        public DatabaseConnectionFailedException(string message, Exception innerException) 
+            : base(message, innerException)
+        {
+        }
+    }
+}
