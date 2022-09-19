74bd8d5cd6e6fd7970eeafabfdf071d8259b2ecc
Daniel Saulin <saulin.daniel33@gmail.com>
2022-09-19 18:13:19 +0300

    Добавил ошибку формата строки подключения к базе данных

---
+++ DatabaseCopierSingle/DatabaseProviders/Exceptions/ConnectionStringFormatException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DatabaseCopierSingle.DatabaseProviders.Exceptions
+{
+    public class ConnectionStringFormatException : ArgumentException
+    {
+        public ConnectionStringFormatException()
+        {
+        }
+
+        public ConnectionStringFormatException(string message)
+            : base(message)
+        {
+        }
+
+        public ConnectionStringFormatException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
