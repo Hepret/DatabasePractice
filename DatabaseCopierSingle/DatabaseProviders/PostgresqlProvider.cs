3ae9111a487885fbb10cdab6f611147f7a52f895
Daniel Saulin <saulin.daniel33@gmail.com>
2022-09-19 18:24:01 +0300

    Реализация метода проверки формата строк

--- DatabaseCopierSingle/DatabaseProviders/PostgresqlProvider.cs
+++ DatabaseCopierSingle/DatabaseProviders/PostgresqlProvider.cs
@@ -1,13 +1,41 @@
 ﻿using Npgsql;
 using System;
+using DatabaseCopierSingle.DatabaseProviders.Exceptions;
 
 namespace DatabaseCopierSingle.DatabaseProviders
 {
     public class PostgresqlProvider : DatabaseProvider
     {
-        public PostgresqlProvider(string connectionString) : base(new NpgsqlConnection(connectionString)) { }
+        public PostgresqlProvider(string connectionString)
+        {
+            ValidateConnectionString(connectionString);
+            var connection = new NpgsqlConnection(connectionString);
+            Conn = connection;
+            try
+            {
+                Conn.Open();
+            }
+            catch (Exception e)
+            {
+                Conn.Close();
+                throw new DatabaseConnectionFailedException($"Can't connect to database, with connection string {Conn.ConnectionString}", e);
+            }
+        }
         
-
+        protected sealed override void ValidateConnectionString(string connectionString)
+        {
+            try
+            {
+                var connectionStringBuilder = new NpgsqlConnectionStringBuilder()
+                {
+                    ConnectionString = connectionString
+                };
+            }
+            catch (Exception e)
+            {
+                throw new ConnectionStringFormatException("Invalid connection string format",e);
+            }
+        }
         public override int ExecuteCommandScalar(string command)
         {
             try
