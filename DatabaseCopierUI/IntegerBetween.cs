027c39aad36801ac985199b97872dc249c6973d4
Daniel Saulin <saulin.daniel33@gmail.com>
2022-09-25 19:33:55 +0300

    Add extension between for integer

---
+++ DatabaseCopierUI/IntegerBetween.cs
@@ -0,0 +1,10 @@
+﻿namespace DatabaseCopierUI
+{
+    public static class IntegerBetween
+    {
+        public static bool Between(this int num, int lower, int upper)
+        {
+            return (num >= lower && num <= upper);
+        }
+    }
+}
