aee1f0a04d2fa0c10c605c86bb5c53db3d13b7b2
Daniel Saulin <saulin.daniel33@gmail.com>
2022-09-25 19:36:49 +0300

    Точка входа в программе

---
+++ DatabaseCopierUI/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DatabaseCopierUI
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Application.Run();
+            }
+            catch (Exception e)
+            {
+                Exception exception = e;
+                while (exception != null)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(exception.Message);
+                    Console.ResetColor();
+                    Console.WriteLine(exception.StackTrace);
+                    Console.WriteLine("##################################################");
+                    exception = exception.InnerException;
+                }
+            }
+        }
+    }
+}
